2dc1e2ed1c281254aafad502b5c8e5604fddd03f
mikiemapo <findmikemm@gmail.com>
2023-04-19 01:37:10 +0000

    remove task set

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,8 @@ import Todo from "./todo";
 //include images into your bundle
 
 //create your first component
+
+
 const Home = () => {
 	return (
 		<div className="text-center">
--- src/js/component/todo.js
+++ src/js/component/todo.js
@@ -1,51 +1,70 @@
 import React, { useEffect, useState } from "react";
 
 export default function Todo() {
+  // list is stored in use state
   const [task, setTask] = useState([]);
+  const [displayx, setDisplayx] = useState(true);
   const [newTodo, setNewTodo] = useState([
     "do something",
     "do something else",
     "do something again",
   ]);
+    // remove task from the list
+  function removeTask(i) {
+    let remItem = newTodo.filter((repItem, repIndex) => {
+      return repIndex != i;
+    });
+    setNewTodo(remItem);
+  
+  }
+  // map is used to output the template in our case its the do something array
   let todoI = newTodo.map((item, index) => {
+ 
     return (
-      <li key={index}>
+      // on mouse enter is the same as hover key property keeps track of each unique item is allways preceeded by map
+      <li
+        onMouseEnter={() => setDisplayx(false)}
+        onMouseLeave={() =>setDisplayx(true)}
+        className="todoItem"
+        key={index}
+      >
         {item}
         <div
-          className="remove"
-          // this where put onclick remove task use filter
+          className={displayx ? "d-none" : "remove"}
+          // remove task use filter
+          onClick={() => {
+            removeTask(index);
+          }}
         >
-          x
+          X
         </div>
       </li>
     );
   });
-
+ // add task to the list . . . is a spread opp
   const addTodo = (onKeyDownEvent) => {
     if (onKeyDownEvent.key === "Enter") {
-        const newTask = [...newTodo, task];
-        setNewTodo(newTask);
-        setTask(""); 
-
+      const newTask = [...newTodo, task];
+      setNewTodo(newTask);
+      setTask("");
     }
-  }
+  };
 
-  return (<div className="spawnC">
-    <h1>Todo List</h1>
-    <div className="Main">
-        <input className="input"
-        placeholder="please enter task" 
-        value={task}
-        onChange = {(event) => setTask(event.target.value)}
-        type="text"
-        onKeyDown={addTodo}
+  return (
+    <div className="spawn">
+      <h1>Todo List</h1>
+      <div className="Main">
+        <input
+          className="input"
+          placeholder="please enter task"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+          type="text"
+          onKeyDown={addTodo}
         />
+      </div>
+      <ul>{todoI}</ul>
+      <div>{todoI.length} task left</div>
     </div>
-    <ul>
-        {todoI}
-    </ul>
-    <div>
-        {todoI.length} task left 
-    </div>
-  </div>);
+  );
 }
